f749d5fec5bbea0ffb8a0fe6dabce5d3dba0e6de
martaboso98 <frboruz@alumnos.ilernasevilla.com>
2023-11-28 11:10:39 +0100

    POR FIN FUNCIONA INGRESAR Y RETIRAR

--- consultas/mostrarMovimientos.php
+++ consultas/mostrarMovimientos.php
@@ -26,34 +26,31 @@ echo "</tr>";
 $saldoAnterior = 0;
 
 while ($fila = mysqli_fetch_assoc($resultadoMovimientos)) {
-    $saldo_decimal = $saldoAnterior;
 
     switch ($monedaUsuario) {
         case "euros":
             $fila['importe'] *= 1;
-            $saldo_decimal += $fila['importe'];
             break;
         case "dolares":
             $fila['importe'] *= 1.1;
-            $saldo_decimal += $fila['importe'];
             break;
         case "libras":
             $fila['importe'] *= 0.9;
-            $saldo_decimal += $fila['importe'];
             break;
         case "yenes":
-            $fila['importe'] *= 160;
-            $saldo_decimal += $fila['importe'];
+            $fila['importe'] /= 160;
             break;
         case "rublos":
-            $fila['importe'] *= 95;
-            $saldo_decimal += $fila['importe'];
+            $fila['importe'] /= 95;
             break;
     }
 
+    // Actualizar el saldo
+    $saldoAnterior += $fila['importe'];
+
     // Aplicar formato de dos decimales
     $importe_formateado = number_format($fila['importe'], 2);
-    $saldo_formateado = number_format($saldo_decimal, 2);
+    $saldo_formateado = number_format($saldoAnterior, 2);
 
     echo "<tr>";
     echo "<td>" . $fila['fecha'] . "</td>";
@@ -62,9 +59,6 @@ while ($fila = mysqli_fetch_assoc($resultadoMovimientos)) {
     echo "<td>" . $saldo_formateado . " " . $monedaUsuario . "</td>";
     echo "</tr>";
 
-    // Update $saldoAnterior for the next iteration
-    $saldoAnterior = $saldo_decimal;
 }
 
 echo "</table>";
-?>
--- consultas/saldo_total.php
+++ consultas/saldo_total.php
@@ -2,72 +2,49 @@
 
 include_once("conexion.php");
 
-//Verificar si se ha iniciado sesión
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
+/* Nombre */
+$dni = $_SESSION['dni'];
 
-$dni = isset($_SESSION['dni']) ? $_SESSION['dni'] : null;
-
-//Obtener moneda del usuario
 $consultaUsuario = "SELECT moneda FROM usuario WHERE dni = '$dni'";
 $resultadoUsuario = mysqli_query($conexion, $consultaUsuario) or die("Error al obtener datos del usuario");
 
 if ($filaUsuario = mysqli_fetch_assoc($resultadoUsuario)) {
     $monedaUsuario = $filaUsuario['moneda'];
-} 
+}
 
-//Obtener saldo actual
-$consultarSaldo = "SELECT saldo_total FROM movimientos WHERE id_cliente='$dni' ORDER BY fecha DESC LIMIT 1";
-$resultadoSaldo = mysqli_query($conexion, $consultarSaldo);
+$consultaMovimientos = "SELECT fecha, concepto, importe FROM movimientos WHERE id_cliente = '$dni'";
+$resultadoMovimientos = mysqli_query($conexion, $consultaMovimientos) or die("Algo ha ido mal en la consulta a la base de datos");
 
 $saldoAnterior = 0;
 
-if ($resultadoSaldo && mysqli_num_rows($resultadoSaldo) > 0) {
-    $filaSaldo = mysqli_fetch_assoc($resultadoSaldo);
+while ($fila = mysqli_fetch_assoc($resultadoMovimientos)) {
 
-    //Ver si no está vacío
-    if (isset($filaSaldo["saldo_total"])) {
-        // Convertir de hexadecimal a decimal
-        $saldoAnterior = hexdec($filaSaldo["saldo_total"]);
-    } 
+    switch ($monedaUsuario) {
+        case "euros":
+            $fila['importe'] *= 1;
+            break;
+        case "dolares":
+            $fila['importe'] *= 1.1;
+            break;
+        case "libras":
+            $fila['importe'] *= 0.9;
+            break;
+        case "yenes":
+            $fila['importe'] /= 160;
+            break;
+        case "rublos":
+            $fila['importe'] /= 95;
+            break;
+    }
+
+    // Actualizar el saldo
+    $saldoAnterior += $fila['importe'];
+
+    // Aplicar formato de dos decimales
+    $importe_formateado = number_format($fila['importe'], 2);
 }
 
-// Inicializar variables
-$importe = 0;
-$concepto = "";
-$accion = ""; 
+// Fuera del bucle, después de procesar todos los movimientos
+$saldo_formateado = number_format($saldoAnterior, 2);
 
-
-
-$importe = isset($_SESSION['importe']) ? $_SESSION['importe'] : null;
-$concepto = isset($_SESSION['concepto']) ? $_SESSION['concepto'] : null;
-
-// Aplicar la conversión de moneda al importe y al saldo total
-switch ($monedaUsuario) {
-    case "euros":
-        // Sin conversión porque la moneda base es euros
-        break;
-    case "dolares":
-        $importe *= 1.1;
-        $saldoAnterior *= 1.1;
-        break;
-    case "libras":
-        $importe *= 0.9;
-        $saldoAnterior *= 0.9;
-        break;
-    case "yenes":
-        $importe /= 160;
-        $saldoAnterior /= 160;
-        break;
-    case "rublos":
-        $importe /= 95;
-        $saldoAnterior /= 95;
-        break;
-}
-
-$saldoTotal = $saldoAnterior + $importe;
-$saldo_formateado = number_format($saldoTotal, 2);
-
-echo $saldoTotal . " " . $monedaUsuario;
-
+echo $saldo_formateado . " " . $monedaUsuario;
