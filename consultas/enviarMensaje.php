4cc0d013cd78778619a97c622d7775443645c863
martaboso98 <frboruz@alumnos.ilernasevilla.com>
2023-11-30 10:23:46 +0100

    enviar mensaje en mensaje y contacto

--- consultas/enviarMensaje.php
+++ consultas/enviarMensaje.php
@@ -10,9 +10,18 @@ if (session_status() == PHP_SESSION_NONE) {
 $dni = isset($_SESSION['dni']) ? $_SESSION['dni'] : null;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $remitente = $_POST["remitente"];
     $destinatario = $_POST["destinatario"];
     $mensaje = $_POST["mensaje"];
+
+    if (isset($_POST["remitente"])) {
+        $remitente = $_POST["remitente"];
+    } else {
+        $consultaRemitente = "SELECT nombre FROM usuario WHERE dni = '$dni'";
+        $resultadoRemitente = mysqli_query($conexion, $consultaRemitente) or die("Algo ha ido mal en la consulta a la base de datos");
+        if ($fila = mysqli_fetch_assoc($resultadoRemitente)) {
+            $remitente = $fila['nombre'];
+        }
+    }
 
     $insertarMensaje = "INSERT INTO mensajes (remitente, destinatario, mensaje) VALUES ('$remitente', '$destinatario', '$mensaje')";
     $resultado_usuario = mysqli_query($conexion, $insertarMensaje);
